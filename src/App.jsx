6ba245712d9aa2674adef39788161bc15de0ddda
Dibyanshoo Pandey <dibyanshoopandey@Dibyanshoos-MacBook-Air.local>
2022-12-26 19:48:36 +0530

    GitHub Top Repo

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CardsContainer from './components/ProjectList/CardsContainer';
+
+const App = () => {
+  return (
+    <div className="App">
+        <CardsContainer />
+    </div>
+  );
+}
+
+export default App;
